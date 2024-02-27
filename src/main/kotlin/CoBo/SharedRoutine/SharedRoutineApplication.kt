dfd0b4c76b18be73ea184064e89d781824a82b06
anjdydhody <a01066444534@gmail.com>
2024-02-27 23:36:28 +0900

    first commit

---
+++ src/main/kotlin/CoBo/SharedRoutine/SharedRoutineApplication.kt
@@ -0,0 +1,11 @@
+package CoBo.SharedRoutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SharedRoutineApplication
+
+fun main(args: Array<String>) {
+	runApplication<SharedRoutineApplication>(*args)
+}
---
+++ src/test/kotlin/CoBo/SharedRoutine/SharedRoutineApplicationTests.kt
@@ -0,0 +1,13 @@
+package CoBo.SharedRoutine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SharedRoutineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
