ee5b229ca5ddc99c84cbddbc96654edd06ae2fb8
trust1204@gmail.com <trust1204@gmail.com>
2024-02-28 16:11:56 +0900

    chore: 사용하지 않는 import 제거

--- src/main/kotlin/CoBo/SharedRoutine/global/repository/QueryDsl/UserRepositoryImpl.kt
+++ src/main/kotlin/CoBo/SharedRoutine/global/repository/QueryDsl/UserRepositoryImpl.kt
@@ -1,12 +1,10 @@
 package CoBo.SharedRoutine.global.repository.QueryDsl
 
-import CoBo.SharedRoutine.global.data.entity.QUser
 import CoBo.SharedRoutine.global.data.entity.QUser.user
 import CoBo.SharedRoutine.global.data.entity.User
 import CoBo.SharedRoutine.global.repository.Custom.UserRepositoryCustom
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
-import javax.sql.DataSource
 
 @Repository
 class UserRepositoryImpl(private val jpaQueryFactory: JPAQueryFactory): UserRepositoryCustom{
