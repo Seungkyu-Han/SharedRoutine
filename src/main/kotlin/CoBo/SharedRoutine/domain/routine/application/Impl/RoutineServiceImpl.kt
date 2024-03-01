2ea22320e7f9bc76c2cae7eb9bae39fb2defd645
anjdydhody <a01066444534@gmail.com>
2024-03-01 11:32:22 +0900

    refactor: Authentication으로 변경

--- src/main/kotlin/CoBo/SharedRoutine/domain/routine/application/Impl/RoutineServiceImpl.kt
+++ src/main/kotlin/CoBo/SharedRoutine/domain/routine/application/Impl/RoutineServiceImpl.kt
@@ -3,7 +3,6 @@ package CoBo.SharedRoutine.domain.routine.application.Impl
 import CoBo.SharedRoutine.domain.routine.Data.Dto.RoutinePostParticipationReq
 import CoBo.SharedRoutine.domain.routine.Data.Dto.RoutinePostReq
 import CoBo.SharedRoutine.domain.routine.application.RoutineService
-import CoBo.SharedRoutine.global.config.jwt.JwtTokenProvider
 import CoBo.SharedRoutine.global.config.response.CoBoResponse
 import CoBo.SharedRoutine.global.config.response.CoBoResponseDto
 import CoBo.SharedRoutine.global.config.response.CoBoResponseStatus
@@ -13,20 +12,19 @@ import CoBo.SharedRoutine.global.repository.RoutineRepository
 import CoBo.SharedRoutine.global.repository.UserRepository
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 
 @Service
 class RoutineServiceImpl(
-    private val jwtTokenProvider: JwtTokenProvider,
     private val userRepository: UserRepository,
     private val routineRepository: RoutineRepository,
     private val participationRepository: ParticipationRepository
 ): RoutineService {
-    override fun post(routinePostReq: RoutinePostReq, authorization: String): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
-        val token = authorization.split(" ")[1]
-        val userId = jwtTokenProvider.getUserId(token)
-        val user = userRepository.findById(userId!!)
+    override fun post(routinePostReq: RoutinePostReq, authentication: Authentication): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
+        val userId = authentication.name.toInt()
+        val user = userRepository.findById(userId)
 
         val routine = Routine(
             id = null,
@@ -40,9 +38,8 @@ class RoutineServiceImpl(
         return CoBoResponse<CoBoResponseStatus>(CoBoResponseStatus.SUCCESS).getResponseEntity()
     }
 
-    override fun postParticipation(routinePostParticipationReq: RoutinePostParticipationReq, authorization: String): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
-        val token = authorization.split(" ")[1]
-        val userId = jwtTokenProvider.getUserId(token)
+    override fun postParticipation(routinePostParticipationReq: RoutinePostParticipationReq, authentication: Authentication): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
+        val userId = authentication.name.toInt()
 
         val routineId = routinePostParticipationReq.routineId
         val routine = routineRepository.findById(routineId)
@@ -50,7 +47,7 @@ class RoutineServiceImpl(
         if (routine.isEmpty)
             return ResponseEntity(HttpStatus.NOT_FOUND)
 
-        if (participationRepository.checkIfRecordExists(userId!!, routineId))
+        if (participationRepository.checkIfRecordExists(userId, routineId))
             return ResponseEntity(HttpStatus.BAD_REQUEST)
 
         participationRepository.insertParticipation(userId, routineId, routinePostParticipationReq.goalDate, routinePostParticipationReq.weekBit.toInt(2))
@@ -58,9 +55,8 @@ class RoutineServiceImpl(
         return CoBoResponse<CoBoResponseStatus>(CoBoResponseStatus.SUCCESS).getResponseEntity()
     }
 
-    override fun patch(routineId: Int, description: String, authorization: String): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
-        val token = authorization.split(" ")[1]
-        val userId = jwtTokenProvider.getUserId(token)
+    override fun patch(routineId: Int, description: String, authentication: Authentication): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
+        val userId = authentication.name.toInt()
 
         val routine = routineRepository.findById(routineId)
 
@@ -76,11 +72,10 @@ class RoutineServiceImpl(
         return CoBoResponse<CoBoResponseStatus>(CoBoResponseStatus.SUCCESS).getResponseEntity()
     }
 
-    override fun patchParticipation(routineId: Int, goalDate: LocalDate, authorization: String): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
-        val token = authorization.split(" ")[1]
-        val userId = jwtTokenProvider.getUserId(token)
+    override fun patchParticipation(routineId: Int, goalDate: LocalDate, authentication: Authentication): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>> {
+        val userId = authentication.name.toInt()
 
-        if (participationRepository.updateGoalDate(userId!!, routineId, goalDate) == 0)
+        if (participationRepository.updateGoalDate(userId, routineId, goalDate) == 0)
             return ResponseEntity(HttpStatus.NOT_FOUND)
 
         return CoBoResponse<CoBoResponseStatus>(CoBoResponseStatus.SUCCESS).getResponseEntity()
