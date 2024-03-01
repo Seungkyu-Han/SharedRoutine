886708d5585086dc6efb5adeb6a86b5295c2bcb6
anjdydhody <a01066444534@gmail.com>
2024-03-01 16:35:57 +0900

    feat: participation post API 추가

--- src/main/kotlin/CoBo/SharedRoutine/global/config/response/CoBoResponseStatus.kt
+++ src/main/kotlin/CoBo/SharedRoutine/global/config/response/CoBoResponseStatus.kt
@@ -6,5 +6,6 @@ enum class CoBoResponseStatus(
 ) {
 
     SUCCESS(2000, "성공"),
-    NOT_FOUND_ELEMENT(4000, "일치하는 요소를 찾을 수 없습니다.")
+    NOT_FOUND_ELEMENT(4000, "일치하는 요소를 찾을 수 없습니다."),
+    CONFLICT(4090, "기존 정보와 충돌합니다.")
 }
