78a9ec87f02bca96d3aec2ba508f813edbf8ad87
anjdydhody <a01066444534@gmail.com>
2024-03-01 12:08:14 +0900

    refactor: Authentication으로 변경

--- src/main/kotlin/CoBo/SharedRoutine/domain/routine/application/RoutineService.kt
+++ src/main/kotlin/CoBo/SharedRoutine/domain/routine/application/RoutineService.kt
@@ -5,8 +5,9 @@ import CoBo.SharedRoutine.domain.routine.Data.Dto.RoutinePostReq
 import CoBo.SharedRoutine.global.config.response.CoBoResponseDto
 import CoBo.SharedRoutine.global.config.response.CoBoResponseStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.Authentication
 
 interface RoutineService {
-    fun post(routinePostReq: RoutinePostReq, authorization: String): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>>
-    fun postParticipation(routinePostParticipationReq: RoutinePostParticipationReq, authorization: String): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>>
+    fun post(routinePostReq: RoutinePostReq, authentication: Authentication): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>>
+    fun postParticipation(routinePostParticipationReq: RoutinePostParticipationReq, authentication: Authentication): ResponseEntity<CoBoResponseDto<CoBoResponseStatus>>
 }
