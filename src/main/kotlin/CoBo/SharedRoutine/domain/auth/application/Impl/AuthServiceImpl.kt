e177869af61c5160341067a68b62e9c7418f2977
anjdydhody <a01066444534@gmail.com>
2024-03-10 22:57:52 +0900

    feat: user nickname patch API 추가

--- src/main/kotlin/CoBo/SharedRoutine/domain/auth/application/Impl/AuthServiceImpl.kt
+++ src/main/kotlin/CoBo/SharedRoutine/domain/auth/application/Impl/AuthServiceImpl.kt
@@ -60,7 +60,7 @@ class AuthServiceImpl(
     }
 
     private fun register(kakaoId: Int): User {
-        val user = User(kakaoId = kakaoId, name = null, refreshToken = null, followerCount = 0, followingCount = 0, image = null)
+        val user = User(kakaoId = kakaoId, name = "게스트"+(-kakaoId), refreshToken = null, followerCount = 0, followingCount = 0, image = null)
         userRepository.save(user)
         return user
     }
